a516a65e35f4bb5504ba2fff1ac95808e0af41b1
Mohan Kumar <mks10685@gmail.com>
2021-03-03 12:21:59 +0530

    added client side javascript

---
+++ public/js/app.js
@@ -0,0 +1,28 @@
+console.log("hello from client");
+
+
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const locationMsg = document.getElementById("location");
+const msgBar = document.getElementById("msgBar");
+const temperature = document.getElementById("temperature");
+const feelsLike = document.getElementById("feels-like");
+const errorMsg = document.getElementById("error");
+
+weatherForm.addEventListener("submit", (event)=>{
+    event.preventDefault();
+    fetch(`http://localhost:5000/weather?address=${search.value}`)
+    .then(res=>res.json()).then(data=>{
+        if(data.error){
+            errorMsg.innerText = data.error
+        }else{
+            msgBar.style.display = "block";
+            locationMsg.innerText = data.location
+            temperature.innerText = data.forecastdata.temp
+            feelsLike.innerText = data.forecastdata.feelslike
+        }
+    });
+
+})
+
+

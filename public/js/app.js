d997458f1fe76ab32f5259187ef7789abf84fd15
Mohan Kumar <mks10685@gmail.com>
2021-03-11 20:09:00 +0530

    updated code

--- public/js/app.js
+++ public/js/app.js
@@ -10,16 +10,22 @@ window.onload = () => {
         getWeather.innerHTML = `<p>Browser does not support geolocation</p>`
     }else{
         navigator.permissions.query({ name: 'geolocation' }).then((result)=>{
+            if(!result){
+                getWeather.innerHTML = `<p>Loading...</p>`
+            }
             if(result.state === "denied"){
                 getWeather.innerHTML= `<p>Geolocation is denied by browser. Please give access to location</p>`
             }else{
                 navigator.geolocation.getCurrentPosition((position) =>{
                     const url = `http://api.weatherstack.com/current?access_key=1d5e2e008d42c7c55663e54842d39575&query=${position.coords.latitude},${position.coords.longitude}&units=m`
                     fetch(url).then(res=>res.json()).then(result=> {
-                        if(result.error){
-                            getWeather.innerHTML= `<p>${result.error}</p>`
+                        if(!result){
+                            getWeather.innerHTML= `<p>Loading....</p>`
                         }else{
-                            getWeather.innerHTML = `<div>
+                            if(result.error){
+                                getWeather.innerHTML= `<p>${result.error}</p>`
+                            }else{
+                                getWeather.innerHTML = `<div>
                                                         <p class="location">${result.location.name}, ${result.location.region},${result.location.country}</p>
                                                     </div>
                                                     <div class="get-weather-data">
@@ -39,6 +45,7 @@ window.onload = () => {
                                                             <p>Wind: <span class="large-font">${result.current.wind_speed} km/hr <span>${result.current.wind_dir}</span></span></p>
                                                         </div>
                                                     </div>`
+                            }
                         }
                     }
                     );
